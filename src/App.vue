b08e99d1db45cddbf3cfc5c1662e557116d88dc1
tuhuaqing <2438974094@qq.com>
2019-09-04 16:19:15 +0800

    你好世界

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <span v-for="(item,index) in list" :key="index">
+        <router-link :to="item.path">{{item.name}}</router-link>|
+      </span>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+// import router from '../router.js'
+export default {
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.list.push(...this.$router.options.routes)
+  }
+}
+</script>
+
 
 <style lang="scss">
 #app {
---
+++ src/views/qrcode.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="box">
+    <qrcode-vue :value="shareUrl" :size="200" class="qrcode" id="picture" ref="code"></qrcode-vue>
+    <a-button type="primary" @click="down"></a-button>
+  </div>
+</template>
+
+<script>
+import qrcode from 'qrcode'
+export default {
+  components: {
+    qrcode
+  },
+  data() {
+    return {
+      shareUrl: 'https://www.baidu.com'
+    }
+  },
+  methods: {
+    down() {
+      //找到canvas标签
+      let myCanvas = document
+        .getElementById('picture')
+        .getElementsByTagName('canvas')
+      //创建一个a标签节点
+      let a = document.createElement('a')
+      //设置a标签的href属性（将canvas变成png图片）
+      a.href = myCanvas[0]
+        .toDataURL('image/png')
+        .replace('image/png', 'image/octet-stream')
+      //设置下载文件的名字
+      a.download = '班级二维码'
+      //点击
+      a.click()
+      //弹出提示
+      this.$message({ message: '亲，正在进行下载保存', type: 'warning' })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
